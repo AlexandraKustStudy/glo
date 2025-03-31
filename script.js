f376c2161a2682db6a73544c8bf75d7c7b00967f
Alexandra <kust997@mail.ru>
2025-03-31 18:16:16 +0300

    Урок 3

--- script.js
+++ script.js
@@ -17,3 +17,36 @@ console.log('Стоимость разработки сайта ' + fullPrice + 
 
 console.log(screens.toLocaleLowerCase().split(', '))
 console.log(fullPrice * (rollback / 100))
+
+title = prompt('Как называется ваш проект?')
+screens = prompt('Какие типы экранов нужно разработать?')
+screenPrice = +prompt('Сколько будет стоить данная работа?', '10000')
+adaptive = confirm('Нужен ли адаптив на сайте?')
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice1 = prompt('Сколько это будет стоить?', '5000')
+let service2 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice2 = prompt('Сколько это будет стоить?', '5000')
+
+fullPrice = screenPrice + parseInt(servicePrice1) + parseInt(servicePrice2);
+
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)))
+console.log(servicePercentPrice)
+
+switch (true) {
+	case fullPrice >= 3000:
+		console.log('Даем скидку в 10%')
+		break
+
+	case fullPrice >= 15000 && fullPrice <= 30000:
+		console.log('Даем скидку в 5%')
+		break
+
+	case fullPrice <= 15000 && fullPrice >= 0:
+		console.log('Скидка не предусмотрена')
+		break
+
+	case fullPrice < 0:
+		console.log('Что то пошло не так')
+		break
+}
