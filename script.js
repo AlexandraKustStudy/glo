cfe3c818d70806b1c2762f9170dacab8509e7c20
Alexandra <kust997@mail.ru>
2025-04-09 22:52:04 +0300

    Урок 7

--- script.js
+++ script.js
@@ -1,96 +1,89 @@
-let title
-let screens
-let screenPrice
-let adaptive
-
-let service1
-let service2
-let fullPrice
-let rollback = 5
-let servicePercentPrice
-let allServicePrices
-
-const isNumber = function (num) {
-	return !isNaN(parseFloat(num)) && isFinite(num)
-}
-
-const asking = function () {
-	title = prompt('Как называется ваш проект?', 'Проект !')
-	screens = prompt('Какие типы экранов нужно разработать?', 'Сложные, простые')
-
-	do {
-		screenPrice = prompt('Сколько будет стоить данная работа?')
-	} while (!isNumber(screenPrice))
-	adaptive = confirm('Нужен ли адаптив на сайте?')
-}
 
 
-let showTypeOf = function (variable) {
-	console.log(variable, typeof variable)
-}
+const appData = {
+	title: '',
+	screens: '',
+	screenPrice: 0,
+	adaptive: false,
+	service1: '',
+	service2: '',
+	fullPrice: 0,
+	rollback: 5,
+	servicePercentPrice: 0,
+	allServicePrices: 0,
+	asking: function () {
+		appData.title = prompt('Как называется ваш проект?', 'Проект !')
+		appData.screens = prompt('Какие типы экранов нужно разработать?', 'Сложные, простые')
 
-let getRollbackMessage = function (price) {
-	switch (true) {
-		case price >= 3000:
-			return 'Даем скидку в 10%'
-			break
+		do {
+			appData.screenPrice = prompt('Сколько будет стоить данная работа?')
+		} while (!appData.isNumber(appData.screenPrice))
+		appData.adaptive = confirm('Нужен ли адаптив на сайте?')
+	},
+	start: function () {
+		appData.asking()
 
-		case price >= 15000 && price <= 30000:
-			return 'Даем скидку в 5%'
-			break
+		appData.allServicePrices = appData.getAllServicePrices()
+		appData.fullPrice = appData.getFullPrice()
+		appData.servicePercentPrice = appData.getServicePercentPrices()
+		appData.title = appData.getTitle()
 
-		case price <= 15000 && price >= 0:
-			return 'Скидка не предусмотрена'
-			break
+		appData.logger()
+	},
+	getRollbackMessage: function (price) {
+		switch (true) {
+			case price >= 3000:
+				return 'Даем скидку в 10%'
+				break
 
-		case price < 0:
-			return 'Что то пошло не так'
-			break
-	}
-}
+			case price >= 15000 && price <= 30000:
+				return 'Даем скидку в 5%'
+				break
 
-let getAllServicePrices = function () {
-	let sum = 0;
+			case price <= 15000 && price >= 0:
+				return 'Скидка не предусмотрена'
+				break
 
-	for (let i = 0; i < 2; i++) {
-		if (i === 0) {
-			service1 = prompt('Какой дополнительный тип услуги нужен?')
-		} else if (i === 1) {
-			service2 = prompt('Какой дополнительный тип услуги нужен?')
+			case price < 0:
+				return 'Что то пошло не так'
+				break
+		}
+	},
+	getAllServicePrices: function () {
+		let sum = 0;
+
+		for (let i = 0; i < 2; i++) {
+			if (i === 0) {
+				appData.service1 = prompt('Какой дополнительный тип услуги нужен?')
+			} else if (i === 1) {
+				appData.service2 = prompt('Какой дополнительный тип услуги нужен?')
+			}
+
+			let answer = prompt('Сколько это будет стоить?')
+
+			if (!appData.isNumber(answer))
+				sum += +answer
 		}
 
-		let answer = prompt('Сколько это будет стоить?')
-
-		if (!isNumber(answer))
-			sum += +answer
-	}
-
-	return sum
+		return sum
+	},
+	getFullPrice: function () {
+		return appData.screenPrice + appData.allServicePrices
+	},
+	getTitle: function () {
+		return appData.title.trim().slice(0, 1).toLocaleUpperCase() + appData.title.trim().slice(1).toLocaleLowerCase()
+	},
+	getServicePercentPrices: function () {
+		return +appData.fullPrice - (+appData.fullPrice * (+appData.rollback / 100))
+	},
+	isNumber: function (num) {
+		return !isNaN(parseFloat(num)) && isFinite(num)
+	},
+	logger: function () {
+		for (let key in appData) {
+			console.log('Ключ: ' + key + ', значение: ' + appData[key])
+		}
+	},
 }
 
-function getFullPrice() {
-	return screenPrice + allServicePrices
-}
-
-function getTitle() {
-	return title.trim().slice(0, 1).toLocaleUpperCase() + title.trim().slice(1).toLocaleLowerCase()
-}
-
-function getServicePercentPrices() {
-	return fullPrice - (fullPrice * (rollback / 100))
-}
-
-asking()
-
-allServicePrices = getAllServicePrices()
-fullPrice = getFullPrice()
-servicePercentPrice = getServicePercentPrices()
-title = getTitle()
-
-showTypeOf(title)
-showTypeOf(fullPrice)
-showTypeOf(adaptive)
-
-console.log(screens.toLocaleLowerCase().split(', '))
-console.log(getRollbackMessage(fullPrice))
-console.log(servicePercentPrice)
+appData.start()
